1172104c39dd52e964a8668543e4be384e66ab03
man7788 <jerman1841@gmail.com>
2024-08-08 10:59:41 +0800

    Remove default routes and view renders

--- api/app.js
+++ api/app.js
@@ -6,9 +6,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
 
 // Set up mongoose connection
@@ -21,18 +18,15 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.get("/", (req, res) => {
+  res.send("odin-book server is up");
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -47,7 +41,6 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
 });
 
 module.exports = app;
