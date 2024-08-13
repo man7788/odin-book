8de0f4ff85ca6aae276230679497d03ac22d5a97
man7788 <jerman1841@gmail.com>
2024-08-14 07:27:54 +0800

    Add index router

--- api/routes/indexRouter.js
+++ api/routes/indexRouter.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
 
+const passport = require("passport");
+const jwtStrategry = require("../config/passport/strategies/jwt");
+passport.use(jwtStrategry);
+
+// GET request for index
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json(req.user.full_name);
+  }
+);
+
 // Post request for user sign-up
 router.post("/signup", indexController.sign_up);
 
