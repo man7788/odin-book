eafeba23d063e8c2b710298376a82f04619a0f91
man7788 <jerman1841@gmail.com>
2024-08-14 07:24:54 +0800

    Add user model virtual

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,11 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  email: { type: String, required: true, minLength: 1, maxLength: 200 },
-  first_name: { type: String, required: true, minLength: 1, maxLength: 100 },
-  last_name: { type: String, required: true, minLength: 1, maxLength: 100 },
-  password: { type: String, required: true, minLength: 8, maxLength: 200 },
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, minLength: 1, maxLength: 200 },
+    first_name: { type: String, required: true, minLength: 1, maxLength: 100 },
+    last_name: { type: String, required: true, minLength: 1, maxLength: 100 },
+    password: { type: String, required: true, minLength: 8, maxLength: 200 },
+  },
+  { toJSON: { virtuals: true } }
+);
+
+UserSchema.virtual("full_name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.last_name) {
+    fullname = `${this.first_name} ${this.last_name}`;
+  }
+
+  return fullname;
 });
 
 // Export model
