77039bebc77d2cc09912aa8fa2d1a45ae8a2188c
man7788 <jerman1841@gmail.com>
2024-08-13 09:30:18 +0800

    Add user login route

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -6,4 +6,7 @@ const userController = require("../controllers/userController");
 // Post request for user sign-up
 router.post("/signup", userController.sign_up);
 
+// Post request for user log-in
+router.post("/login", userController.log_in);
+
 module.exports = router;
