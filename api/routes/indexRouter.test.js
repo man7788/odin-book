39d743464cfe9b9a8e63edd1be74022605262ea4
man7788 <jerman1841@gmail.com>
2024-08-15 01:11:18 +0800

    Add successful login test

--- api/routes/indexRouter.test.js
+++ api/routes/indexRouter.test.js
@@ -8,6 +8,7 @@ require("dotenv").config({ path: "./config/tests/.env.test" });
 const request = require("supertest");
 const express = require("express");
 const indexRouter = require("./indexRouter");
+const User = require("../models/userModel");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/", indexRouter);
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 beforeAll(async () => {
   await connectDB();
@@ -29,11 +31,44 @@ afterEach(async () => {
   jest.clearAllMocks();
 });
 
+jest.mock("bcryptjs");
+
 describe("index route", () => {
   test("should response with 401 Unauthorized", async () => {
     const response = await request(app).get("/");
 
-    expect(response.text).toMatch("Unauthorized");
     expect(response.status).toEqual(401);
+    expect(response.text).toMatch("Unauthorized");
+  });
+
+  test("should response with login user's full name", async () => {
+    bcrypt.compare.mockImplementationOnce(() => true);
+
+    const userId = new mongoose.Types.ObjectId();
+
+    const user = new User({
+      email: "john@doe.com",
+      first_name: "john",
+      last_name: "doe",
+      password: "johndoefoobar",
+      _id: userId,
+    });
+    await user.save();
+
+    const payload = { email: "john@doe.com", password: "johndoefoobar" };
+
+    const login = await request(app)
+      .post("/login")
+      .set("Content-Type", "application/json")
+      .send(payload);
+
+    const token = login.body.token;
+
+    const response = await request(app)
+      .get("/")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toEqual(200);
+    expect(response.text).toMatch("john doe");
   });
 });
