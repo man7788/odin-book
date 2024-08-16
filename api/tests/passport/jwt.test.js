2dbbd3f661657705bc3854b2f7706bb926f247b4
man7788 <jerman1841@gmail.com>
2024-08-16 11:57:38 +0800

    Add jwt test

--- api/tests/passport/jwt.test.js
+++ api/tests/passport/jwt.test.js
@@ -1,17 +1,55 @@
 require("dotenv").config({ path: ".env.test.local" });
 
+const {
+  connectDB,
+  dropDB,
+  dropCollections,
+} = require("../../utils/mongoMemoryServer/setuptestdb");
+const mongoose = require("mongoose");
+const User = require("../../models/userModel");
+
 const request = require("supertest");
 const express = require("express");
 const app = express();
 
 const passport = require("passport");
 const jwtStrategry = require("../../utils/passport/jwt");
+const jwt = require("jsonwebtoken");
 passport.use(jwtStrategry);
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await dropDB();
+});
+
+afterEach(async () => {
+  await dropCollections();
+  jest.clearAllMocks();
+});
+
+const userId = new mongoose.Types.ObjectId();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/", passport.authenticate("jwt", { session: false }), (req, res) => {
+
+app.get("/", passport.authenticate("jwt", { session: false }), (req, res) => {
   res.send("login successful");
+});
+
+app.post("/login", (req, res) => {
+  jwt.sign({ sub: userId }, process.env.JWT_SECRET, async (err, token) => {
+    if (err) {
+      return next(err);
+    }
+    try {
+      res.json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  });
 });
 
 describe("jwt strategy", () => {
@@ -20,5 +58,26 @@ describe("jwt strategy", () => {
 
     expect(response.status).toEqual(401);
     expect(response.text).toMatch("Unauthorized");
+  });
+
+  test("should response with login successful", async () => {
+    const user = new User({
+      email: "foo@bar.com",
+      first_name: "foo",
+      last_name: "bar",
+      password: "foobar123",
+      _id: userId,
+    });
+    await user.save();
+    await User.findOne({ email: "foo@bar.com" });
+
+    const login = await request(app).post("/login");
+
+    const response = await request(app)
+      .get("/")
+      .set("Authorization", `Bearer ${login.body.token}`);
+
+    expect(response.status).toEqual(200);
+    expect(response.text).toMatch("login successful");
   });
 });
