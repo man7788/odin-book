bbdd570b407189bc3be4545752a339e22726e46b
man7788 <jerman1841@gmail.com>
2024-08-16 10:14:31 +0800

    Update index route tests

--- api/routes/indexRouter.test.js
+++ api/routes/indexRouter.test.js
@@ -3,20 +3,15 @@ const {
   dropDB,
   dropCollections,
 } = require("../config/tests/setuptestdb");
-require("dotenv").config({ path: "./config/tests/.env.test" });
 
 const request = require("supertest");
 const express = require("express");
 const indexRouter = require("./indexRouter");
-const User = require("../models/userModel");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", indexRouter);
-
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
 beforeAll(async () => {
   await connectDB();
@@ -32,43 +27,22 @@ afterEach(async () => {
 });
 
 jest.mock("bcryptjs");
+jest.mock("../config/passport/strategies/jwt", () => new Object());
+jest.mock("passport", () => {
+  return {
+    use: jest.fn(),
+    authenticate: jest.fn(() => (req, res, next) => {
+      req.user = { full_name: "foobar" };
+      next();
+    }),
+  };
+});
 
 describe("index route", () => {
-  test("should response with 401 Unauthorized", async () => {
+  test("should response with login user's full name", async () => {
     const response = await request(app).get("/");
 
-    expect(response.status).toEqual(401);
-    expect(response.text).toMatch("Unauthorized");
-  });
-
-  test("should response with login user's full name", async () => {
-    bcrypt.compare.mockImplementationOnce(() => true);
-
-    const userId = new mongoose.Types.ObjectId();
-
-    const user = new User({
-      email: "john@doe.com",
-      first_name: "john",
-      last_name: "doe",
-      password: "johndoefoobar",
-      _id: userId,
-    });
-    await user.save();
-
-    const payload = { email: "john@doe.com", password: "johndoefoobar" };
-
-    const login = await request(app)
-      .post("/login")
-      .set("Content-Type", "application/json")
-      .send(payload);
-
-    const token = login.body.token;
-
-    const response = await request(app)
-      .get("/")
-      .set("Authorization", `Bearer ${token}`);
-
     expect(response.status).toEqual(200);
-    expect(response.text).toMatch("john doe");
+    expect(response.body).toMatchObject({ full_name: expect.any(String) });
   });
 });
