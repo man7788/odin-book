331829a46832d073f0a89af8064b5110aa8eadba
man7788 <jerman1841@gmail.com>
2024-08-26 12:06:16 +0800

    Add error test

---
+++ api/tests/error/error.test.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+const express = require("express");
+
+const app = express();
+
+app.use("/error404", (req, res, next) => {
+  const error = new Error("404 error message");
+  error.status = 404;
+  throw error;
+});
+
+app.use("/error", (req, res, next) => {
+  throw new Error("error message");
+});
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).send(err.message);
+});
+
+describe("error handle middleware", () => {
+  test("should response with status 500 error", async () => {
+    const response = await request(app).get("/error");
+    expect(response.status).toEqual(500);
+    expect(response.error.text).toEqual("error message");
+  });
+});
