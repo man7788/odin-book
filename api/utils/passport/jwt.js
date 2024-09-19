c604cbf9bb9d7ace2124ffa674701dfa3849d722
man7788 <jerman1841@gmail.com>
2024-09-19 11:15:43 +0800

    Update jwt strategy

--- api/utils/passport/jwt.js
+++ api/utils/passport/jwt.js
@@ -5,12 +5,14 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.JWT_SECRET;
+opts.passReqToCallback = true;
 
-module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
+module.exports = new JwtStrategy(opts, async (req, jwt_payload, done) => {
   try {
     const user = await User.findOne({ _id: jwt_payload.sub });
 
     if (user) {
+      req.user = user;
       return done(null, user);
     } else {
       return done(null, false);
