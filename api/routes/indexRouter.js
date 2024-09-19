74d7a6f057124d2690456e72da00945053cb46b1
man7788 <jerman1841@gmail.com>
2024-09-19 11:17:01 +0800

    Add passport authentication

--- api/routes/indexRouter.js
+++ api/routes/indexRouter.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
 
+const passport = require("passport");
+const jwtStrategry = require("../utils/passport/jwt");
+passport.use(jwtStrategry);
+
 // GET request for index
-router.get("/", indexController.index);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  indexController.index
+);
 
 // Post request for user sign-up
 router.post("/signup", indexController.sign_up);
