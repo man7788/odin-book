c860411c0ae9fcbf85fb72a395dbb0f0cca0ecfa
man7788 <jerman1841@gmail.com>
2024-09-29 01:35:24 +0800

    Add followers route middleware

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var logger = require("morgan");
 const cors = require("cors");
 
 const indexRouter = require("./routes/indexRouter");
+const followerRouter = require("./routes/followerRouter");
 
 var app = express();
 app.use(cors());
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/followers", followerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
