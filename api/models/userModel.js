8ce96a6e661efde6624e9dcb2816bf31546a0a3c
man7788 <jerman1841@gmail.com>
2024-09-28 14:15:34 +0800

    Update user model

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,23 +2,10 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema(
-  {
-    email: { type: String, required: true, minLength: 1, maxLength: 200 },
-    first_name: { type: String, required: true, minLength: 1, maxLength: 100 },
-    last_name: { type: String, required: true, minLength: 1, maxLength: 100 },
-    password: { type: String, required: true, minLength: 8, maxLength: 200 },
-  },
-  { toJSON: { virtuals: true } }
-);
-
-UserSchema.virtual("full_name").get(function () {
-  let fullname = "";
-  if (this.first_name && this.last_name) {
-    fullname = `${this.first_name} ${this.last_name}`;
-  }
-
-  return fullname;
+const UserSchema = new Schema({
+  email: { type: String, required: true, minLength: 1, maxLength: 200 },
+  password: { type: String, required: true, minLength: 8, maxLength: 200 },
+  profile: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
 });
 
 // Export model
