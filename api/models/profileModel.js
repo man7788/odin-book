1a2dca987d0e98b3706453f199b35a679b5aa8c3
man7788 <jerman1841@gmail.com>
2024-09-28 14:15:42 +0800

    Add profile model

---
+++ api/models/profileModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema(
+  {
+    first_name: { type: String, required: true, minLength: 1, maxLength: 25 },
+    last_name: { type: String, required: true, minLength: 1, maxLength: 25 },
+  },
+  { toJSON: { virtuals: true } }
+);
+
+ProfileSchema.virtual("full_name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.last_name) {
+    fullname = `${this.first_name} ${this.last_name}`;
+  }
+
+  return fullname;
+});
+
+// Export model
+module.exports = mongoose.model("Profile", ProfileSchema);
