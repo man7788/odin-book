a6eb66d56e3ab4c4e3ac5f34a45097d5c14fe0bb
man7788 <jerman1841@gmail.com>
2024-09-29 01:34:21 +0800

    Add follower controller

---
+++ api/controllers/followerController.js
@@ -0,0 +1,32 @@
+const asyncHandler = require("express-async-handler");
+const Request = require("../models/requestModel");
+const Follower = require("../models/followerModel");
+const Profile = require("../models/profileModel");
+
+// Handle friend request create on POST
+exports.request_create = asyncHandler(async (req, res, next) => {
+  const profile = await Profile.findById(req.user.profile);
+
+  const follower = await Follower.findOne({
+    follower: profile._id,
+    following: req.body.following_id,
+  });
+
+  const request = await Request.findOne({
+    from: profile._id,
+    to: req.body.following_id,
+  });
+
+  if (follower || request) {
+    res.json(null);
+  } else {
+    const request = new Request({
+      from: profile._id,
+      to: req.body.following_id,
+    });
+
+    const createdRequest = await request.save();
+
+    res.json({ createdRequest });
+  }
+});
