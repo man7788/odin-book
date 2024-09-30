daa82e88e681a3a50ff553d45a57494a86c3ce65
man7788 <jerman1841@gmail.com>
2024-10-01 01:27:08 +0800

    Add post like create controller

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,7 +1,9 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
+const mongoose = require("mongoose");
 const Profile = require("../models/profileModel");
 const Post = require("../models/postModel");
+const Like = require("../models/likeModel");
 
 // Handle post create on POST
 exports.post_create = [
@@ -34,6 +36,51 @@ exports.post_create = [
         const createdPost = await post.save();
 
         res.json({ createdPost: createdPost._id });
+      }
+    }
+  }),
+];
+
+// Handle post like on POST
+exports.post_like = [
+  body("post_id")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Post id must not be empty")
+    .custom((value) => {
+      const validId = mongoose.isValidObjectId(value);
+      if (!validId) {
+        throw new Error("Invalid post ID");
+      }
+      return validId;
+    })
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+    } else {
+      const post = await Post.findById(req.body.post_id);
+      const alreadyLiked = await Like.findOne({ post: req.body.post_id });
+      const profile = await Profile.findOne(req.user.profile);
+
+      if (!post || alreadyLiked) {
+        res.json(null);
+      } else {
+        const like = new Like({
+          post: req.body.post_id,
+          profile: req.user.profile,
+          author: profile.full_name,
+        });
+
+        const createdLike = await like.save();
+
+        res.json({
+          createdLike: createdLike._id,
+        });
       }
     }
   }),
