1ae4c69a725909febad7624045e4646e66a7aef2
man7788 <jerman1841@gmail.com>
2024-10-01 02:15:17 +0800

    Update user model

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   email: { type: String, required: true, minLength: 1, maxLength: 200 },
   password: { type: String, required: true, minLength: 8, maxLength: 200 },
-  profile: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
+  profile: { type: Schema.Types.ObjectId, required: true, ref: "Profile" },
 });
 
 // Export model
