42c9e711c2c01c63946f61b74da6c3b1708983b1
man7788 <jerman1841@gmail.com>
2024-10-01 10:27:22 +0800

    Update post model

--- api/models/postModel.js
+++ api/models/postModel.js
@@ -2,11 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-  profile: { type: Schema.Types.ObjectId, required: true, ref: "Profile" },
-  author: { type: String, required: true, minLength: 1, maxLength: 51 },
-  text_content: { type: String, required: true, minLength: 1, maxLength: 280 },
-});
+const PostSchema = new Schema(
+  {
+    profile: { type: Schema.Types.ObjectId, required: true, ref: "Profile" },
+    author: { type: String, required: true, minLength: 1, maxLength: 51 },
+    text_content: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+  },
+  { timestamps: true }
+);
 
 // Export model
 module.exports = mongoose.model("Post", PostSchema);
