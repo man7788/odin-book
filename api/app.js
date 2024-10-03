a38abe6f3ab12c4c117a19c74a165a4225af98a8
man7788 <jerman1841@gmail.com>
2024-10-03 09:50:36 +0800

    Add user router

--- api/app.js
+++ api/app.js
@@ -32,7 +32,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/user", userRouter);
 app.use("/users", userRouter);
 app.use("/followers", followerRouter);
 app.use("/posts", postRouter);
