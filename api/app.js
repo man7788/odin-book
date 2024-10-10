2a0b77e4a7a5abe1375fc2e72700acb41752f394
man7788 <jerman1841@gmail.com>
2024-10-10 08:34:53 +0800

    Format to Airbnb style

--- api/app.js
+++ api/app.js
@@ -1,48 +1,50 @@
-require("dotenv").config();
+require('dotenv').config();
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const cors = require("cors");
-
-const indexRouter = require("./routes/indexRouter");
-const userRouter = require("./routes/userRouter");
-const followerRouter = require("./routes/followerRouter");
-const postRouter = require("./routes/postRouter");
-
-var app = express();
-app.use(cors());
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
 // Set up mongoose connection
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URI;
 
-main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
+main().catch((err) => console.log(err));
 
-app.use(logger("dev"));
+const indexRouter = require('./routes/indexRouter');
+const userRouter = require('./routes/userRouter');
+const followerRouter = require('./routes/followerRouter');
+const postRouter = require('./routes/postRouter');
+
+const app = express();
+app.use(cors());
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", userRouter);
-app.use("/followers", followerRouter);
-app.use("/posts", postRouter);
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+app.use('/followers', followerRouter);
+app.use('/posts', postRouter);
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // Error handler
-app.use(function (err, req, res, next) {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message });
 });
 
