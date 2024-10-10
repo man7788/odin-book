6c64ad3ee36983f4f5a69a2db8f8e25493409b85
man7788 <jerman1841@gmail.com>
2024-10-10 08:35:44 +0800

    Format to Airbnb style

--- api/utils/passport/jwt.js
+++ api/utils/passport/jwt.js
@@ -1,25 +1,26 @@
-const User = require("../../models/userModel");
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.JWT_SECRET;
 opts.passReqToCallback = true;
 
+const User = require('../../models/userModel');
+
 module.exports = new JwtStrategy(opts, async (req, jwt_payload, done) => {
   try {
     const user = await User.findOne(
       { _id: jwt_payload.sub },
-      "profile"
-    ).populate("profile");
+      'profile',
+    ).populate('profile');
 
     if (user) {
       req.user = user;
       return done(null, user);
-    } else {
-      return done(null, false);
     }
+
+    return done(null, false);
   } catch (err) {
     return done(err, false);
   }
