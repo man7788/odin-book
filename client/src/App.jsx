509a2b153403422f240b39be38184addc9f1bd70
man7788 <jerman1841@gmail.com>
2024-10-16 03:37:36 +0800

    Add index page auth and redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,36 @@
-import styles from "./App.module.css";
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+import useAuth from './hooks/useAuth';
+import { Navigate } from 'react-router-dom';
 
 function App() {
-  return <div className={styles.App}>Odin Book</div>;
+  const { authResult, authLoading, authError } = useAuth();
+  const [navLogin, setNavLogin] = useState(false);
+  const [serverError, setServerError] = useState(false);
+
+  useEffect(() => {
+    if (authError?.message === 'Unauthorized') {
+      setNavLogin(true);
+    } else if (authError) {
+      setServerError(true);
+    }
+  }, [authError]);
+
+  if (authLoading) {
+    return <div className={styles.App}>Loading...</div>;
+  }
+
+  if (serverError) {
+    return <div className={styles.App}>Server Error</div>;
+  }
+
+  return (
+    <div className={styles.App}>
+      Odin Book<br></br>
+      {authResult?.full_name}
+      {navLogin && <Navigate to="/login" replace={true} />}
+    </div>
+  );
 }
 
 export default App;
