2e66572b1125c4b7140da53f5136763d36aa1add
man7788 <jerman1841@gmail.com>
2024-10-16 03:33:55 +0800

    Update error handler response json

--- api/app.js
+++ api/app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 // Error handler
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({ error: err.message });
+  res.status(err.status || 500).json(err);
 });
 
 module.exports = app;
