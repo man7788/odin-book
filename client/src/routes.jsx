934004aa5f42e164c3af4bbc39a5ef3ece4aec8b
man7788 <jerman1841@gmail.com>
2024-10-16 03:37:03 +0800

    Add Login route

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,10 +1,16 @@
 import App from "./App";
+import Login from "./components/Login";
 import ErrorPage from "./ErrorPage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
     errorElement: <ErrorPage />,
   },
 ];
