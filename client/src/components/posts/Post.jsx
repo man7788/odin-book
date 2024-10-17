bfe693800228483f87c7656339c4f045e0dbb07d
man7788 <jerman1841@gmail.com>
2024-10-18 03:48:41 +0800

    Add link to show username profile

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -1,13 +1,14 @@
 import styles from './PostList.module.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Comment from './Comment';
 
 function Post(props) {
-  const { author, text_content, likes, comments } = props;
+  const { profile, author, text_content, likes, comments } = props;
 
   return (
     <div className={styles.PostList} style={{ border: '2px solid orange' }}>
-      {author}
+      <Link to={`/${profile}`}>{author}</Link>
       <br></br>
       {text_content}
       <br></br>
@@ -24,6 +25,7 @@ function Post(props) {
 }
 
 Post.propTypes = {
+  profile: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   text_content: PropTypes.string.isRequired,
   likes: PropTypes.arrayOf(PropTypes.object).isRequired,
