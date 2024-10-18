16dc7723f887aecf580ae84197ea8acf60960bfa
man7788 <jerman1841@gmail.com>
2024-10-18 12:24:31 +0800

    Create likeFetch

---
+++ client/src/fetch/likeFetch.jsx
@@ -0,0 +1,30 @@
+const likeFetch = async (postId) => {
+  const token = JSON.parse(localStorage.getItem('token'));
+
+  try {
+    const response = await fetch(
+      `http://localhost:3000/posts/${postId}/likes`,
+      {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    if (response.status >= 400) {
+      console.log(response.status);
+      const error = new Error(response.statusText);
+      error.code = response.status;
+      throw error;
+    }
+
+    const result = await response.json();
+
+    return { result };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export default likeFetch;
