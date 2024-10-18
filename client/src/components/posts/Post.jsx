f755900ede30cc3d1ef54e18dbc8233f08703088
man7788 <jerman1841@gmail.com>
2024-10-18 12:24:39 +0800

    Add like button

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -1,10 +1,40 @@
 import styles from './PostList.module.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import likeFetch from '../../fetch/likeFetch';
 import Comment from './Comment';
 
 function Post(props) {
-  const { profile, author, text_content, likes, comments } = props;
+  const { profile, author, text_content, likes, comments, _id } = props;
+  const [loading, setLoading] = useState(false);
+  const [serverError, setServerError] = useState(null);
+
+  const onLike = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(_id);
+
+    const { result, error } = await likeFetch(_id);
+
+    if (error?.code) {
+      setServerError(true);
+    }
+
+    if (result?.createdLike) {
+      console.log('liked');
+    }
+
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <div className={styles.App}>Loading...</div>;
+  }
+
+  if (serverError) {
+    return <div className={styles.App}>Server Error</div>;
+  }
 
   return (
     <div className={styles.PostList} style={{ border: '2px solid orange' }}>
@@ -12,8 +42,9 @@ function Post(props) {
       <br></br>
       {text_content}
       <br></br>
-      {'Likes: '}
-      {likes ? likes?.length : 0}
+      <button onClick={onLike}>Like</button>
+      <br></br>
+      {likes ? likes?.length : 0} Likes
       <br></br>
       {'Comments: '}
       <br></br>
@@ -30,6 +61,7 @@ Post.propTypes = {
   text_content: PropTypes.string.isRequired,
   likes: PropTypes.arrayOf(PropTypes.object).isRequired,
   comments: PropTypes.arrayOf(PropTypes.object).isRequired,
+  _id: PropTypes.string.isRequired,
 };
 
 export default Post;
