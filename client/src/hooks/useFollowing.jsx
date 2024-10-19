9308da1d90c33ed006bbf73748169cb285cc5850
man7788 <jerman1841@gmail.com>
2024-10-20 03:53:53 +0800

    Add useFollowing hook

---
+++ client/src/hooks/useFollowing.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+const useFollowing = (profileId) => {
+  const [followingResult, setFollowingResult] = useState(null);
+  const [followingLoading, setFollowingLoading] = useState(true);
+  const [followingError, setFollowingError] = useState(null);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    const fetchStatus = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/followers/following/${profileId}`,
+          {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        if (response.status >= 400) {
+          const error = new Error(response.statusText);
+          error.code = response.status;
+          throw error;
+        }
+
+        const responseData = await response.json();
+
+        setFollowingResult(responseData);
+      } catch (error) {
+        setFollowingError(error);
+      } finally {
+        setFollowingLoading(false);
+      }
+    };
+    fetchStatus();
+  }, [profileId]);
+
+  return { followingResult, followingLoading, followingError };
+};
+
+export default useFollowing;
