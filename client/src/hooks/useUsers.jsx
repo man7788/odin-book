229b3dbfa7c25bfdc7b5d82e90e04b9e20f1348f
man7788 <jerman1841@gmail.com>
2024-10-22 03:48:06 +0800

    Create useUsers hook

---
+++ client/src/hooks/useUsers.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+const useUsers = () => {
+  const [usersResult, setUsersResult] = useState(null);
+  const [usersLoading, setUsersLoading] = useState(true);
+  const [usersError, setUsersError] = useState(null);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    const fetchStatus = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/users`, {
+          mode: 'cors',
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.status >= 400) {
+          const error = new Error(response.statusText);
+          error.code = response.status;
+          throw error;
+        }
+
+        const responseData = await response.json();
+
+        setUsersResult(responseData);
+      } catch (error) {
+        setUsersError(error);
+      } finally {
+        setUsersLoading(false);
+      }
+    };
+    fetchStatus();
+  }, []);
+
+  return { usersResult, usersLoading, usersError };
+};
+
+export default useUsers;
