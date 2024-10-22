8ec5bea14b1ee33b467db5fb48f67bb079fe6039
man7788 <jerman1841@gmail.com>
2024-10-23 01:22:53 +0800

    Create signUpFetch

---
+++ client/src/fetch/signUpFetch.jsx
@@ -0,0 +1,31 @@
+const signUpFetch = async (loginPayload) => {
+  try {
+    const response = await fetch(`http://localhost:3000/signup`, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginPayload),
+    });
+
+    if (response.status === 400) {
+      const errors = await response.json();
+      throw errors;
+    }
+
+    if (response.status > 400) {
+      const error = new Error(response.statusText);
+      error.code = response.status;
+      throw error;
+    }
+
+    const result = await response.json();
+
+    return { result };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export default signUpFetch;
