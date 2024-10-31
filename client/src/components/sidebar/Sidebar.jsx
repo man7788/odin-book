edc8db55b65476ffbe7aaeb0e7e3e524f2a0abe0
man7788 <jerman1841@gmail.com>
2024-10-31 11:57:32 +0800

    Add render links

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,33 @@
-import { Link } from 'react-router-dom';
 import styles from './Sidebar.module.css';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 
-function Sidebar() {
+function Sidebar({ fullName }) {
+  const [navLogin, setNavLogin] = useState(false);
+
+  const onLogout = () => {
+    localStorage.clear();
+    setNavLogin(true);
+  };
+
   return (
     <div className={styles.Sidebar}>
+      <Link to="/">Odin Book</Link>
       <Link to="/">Home</Link>
       <Link to="users">Users</Link>
       <Link to="users/requests">Requests</Link>
+      <div className={styles.fullName}>{fullName}</div>
+      <Link onClick={onLogout} to="/login">
+        Log Out
+      </Link>
+      {navLogin && <Navigate to="/login" replace={true} />}
     </div>
   );
 }
+
+Sidebar.propTypes = {
+  fullName: PropTypes.string,
+};
 
 export default Sidebar;
