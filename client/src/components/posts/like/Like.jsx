2d7318c962718b72d7df3ae7b47f0c30f2320ccd
man7788 <jerman1841@gmail.com>
2024-11-02 03:51:33 +0800

    Update like click function

--- client/src/components/posts/like/Like.jsx
+++ client/src/components/posts/like/Like.jsx
@@ -1,15 +1,28 @@
 import styles from './Like.module.css';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import likeFetch from '../../../fetch/likeFetch';
 
 function Like({ postId, likes }) {
-  const [loading, setLoading] = useState(false);
+  const { profile } = useOutletContext();
+  const [localLikes, setLocalLikes] = useState(likes.length);
+  const [liked, setLiked] = useState(false);
   const [serverError, setServerError] = useState(null);
+
+  useEffect(() => {
+    const likeList = [];
+    likes.forEach((like) => {
+      likeList.push(like.profile);
+    });
+
+    if (likeList.includes(profile)) {
+      setLiked(true);
+    }
+  }, [likes, profile]);
 
   const onLike = async (e) => {
     e.preventDefault();
-    setLoading(true);
 
     const { result, error } = await likeFetch(postId);
 
@@ -17,27 +30,39 @@ function Like({ postId, likes }) {
       setServerError(true);
     }
 
-    if (result) {
-      console.log(result);
+    if (error) {
+      setServerError(true);
     }
 
-    setLoading(false);
+    if (result.createdLike) {
+      setLiked(true);
+      setLocalLikes(localLikes + 1);
+    }
+
+    if (result.removedLike) {
+      setLiked(false);
+      if (localLikes > 1) {
+        setLocalLikes(localLikes - 1);
+      } else {
+        setLocalLikes(0);
+      }
+    }
   };
 
-  if (loading) {
-    return <div className={styles.App}>Loading...</div>;
-  }
-
   if (serverError) {
-    return <div className={styles.App}>Server Error</div>;
+    return <div className={styles.Like}>Server Error</div>;
   }
 
   return (
     <div className={styles.Like}>
-      {likes.length === 0 || likes.length === 1 ? (
-        <button onClick={onLike}>{likes.length} Like</button>
+      {!liked ? (
+        localLikes === 0 || localLikes === 1 ? (
+          <button onClick={onLike}>{localLikes} Like</button>
+        ) : (
+          <button onClick={onLike}>{localLikes} Likes</button>
+        )
       ) : (
-        <button onClick={onLike}>{likes.length} Likes</button>
+        <button onClick={onLike}>{localLikes} Unlike</button>
       )}
     </div>
   );
