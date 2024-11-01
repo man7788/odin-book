e0eeb6ed4ea92d619db6aa4c3e8816acbf9298f7
man7788 <jerman1841@gmail.com>
2024-11-01 11:06:51 +0800

    Remove render Create

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,9 @@
 import styles from './Home.module.css';
-import Create from '../create/Create';
 import PostList from '../posts/PostList';
 
 function Home() {
   return (
     <div className={styles.Home}>
-      <Create />
       <PostList />
     </div>
   );
