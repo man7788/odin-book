4bd787e8083a0abba5ae3a8e5d18f908fc59fef9
man7788 <jerman1841@gmail.com>
2024-11-10 07:30:10 +0800

    Update index router test

--- api/tests/routes/indexRouter.test.js
+++ api/tests/routes/indexRouter.test.js
@@ -31,6 +31,7 @@ afterEach(async () => {
   jest.clearAllMocks();
 });
 
+const mockProfileId1 = new mongoose.Types.ObjectId('507f1f77bcf86cd799439011');
 const profileId = new mongoose.Types.ObjectId();
 
 jest.mock('bcryptjs');
@@ -39,7 +40,7 @@ jest.mock('../../utils/passport/jwt', () => {});
 jest.mock('passport', () => ({
   use: jest.fn(),
   authenticate: jest.fn(() => (req, res, next) => {
-    req.user = { profile: { full_name: 'foobar' } };
+    req.user = { profile: { full_name: 'foobar', _id: mockProfileId1 } };
     next();
   }),
 }));
@@ -50,7 +51,13 @@ describe('index router', () => {
       const response = await request(app).get('/');
 
       expect(response.status).toEqual(200);
-      expect(response.body).toMatchObject({ full_name: expect.any(String) });
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          full_name: expect.any(String),
+          profile: expect.any(String),
+        }),
+      );
+      expect(mongoose.isValidObjectId(response.body.profile)).toBeTruthy();
     });
   });
 
