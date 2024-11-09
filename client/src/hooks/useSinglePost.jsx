f8e65a4470bbff9a0988b15ab5c2167072be4939
man7788 <jerman1841@gmail.com>
2024-11-10 00:21:11 +0800

    Create useSinglePost hook

---
+++ client/src/hooks/useSinglePost.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+const useSinglePost = (profileId) => {
+  const [postResult, setPostResult] = useState(null);
+  const [postLoading, setPostLoading] = useState(true);
+  const [postError, setPostError] = useState(null);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/posts/${profileId}`,
+          {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        if (response.status >= 400) {
+          const error = new Error(response.statusText);
+          error.code = response.status;
+          throw error;
+        }
+
+        const responseData = await response.json();
+
+        setPostResult(responseData);
+      } catch (error) {
+        setPostError(error);
+      } finally {
+        setPostLoading(false);
+      }
+    };
+    fetchPosts();
+  }, [profileId]);
+
+  return { postResult, postLoading, postError };
+};
+
+export default useSinglePost;
