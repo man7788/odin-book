581301a8194753e76130fd991e5be6f8a9aae55e
man7788 <jerman1841@gmail.com>
2024-11-16 07:44:57 +0800

    Update render

--- client/src/ErrorPage.jsx
+++ client/src/ErrorPage.jsx
@@ -1,13 +1,34 @@
-import { Link } from "react-router-dom";
+import styles from './ErrorPage.module.css';
+import { Link } from 'react-router-dom';
+import App from './App';
+import useAuth from './hooks/useAuth';
 
 const ErrorPage = () => {
+  const { authResult, authLoading, authError } = useAuth();
+
+  if (authLoading) {
+    return (
+      <div className={styles.loading}>
+        <div className={styles.loader}></div>
+      </div>
+    );
+  }
+
+  if (authResult) {
+    return <>{authResult && <App errorRedirect={true} />}</>;
+  }
+
   return (
-    <div>
-      <h1>Oh no, this page doesn&apos;t exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
-    </div>
+    <>
+      {authError && (
+        <div className={styles.error}>
+          <h1>Oh no, this page doesn&apos;t exist!</h1>
+          <Link to="/">
+            You can go back to the home page by clicking here, though!
+          </Link>
+        </div>
+      )}
+    </>
   );
 };
 
