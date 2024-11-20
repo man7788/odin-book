f74d75c17e272a55dae3eb36f96e5cd0033fe02c
man7788 <jerman1841@gmail.com>
2024-11-20 08:44:36 +0800

    Add useProfile hook

--- client/src/components/avatar/Avatar.jsx
+++ client/src/components/avatar/Avatar.jsx
@@ -1,20 +1,40 @@
 import styles from './Avatar.module.css';
 import PropTypes from 'prop-types';
 import CryptoJS from 'crypto-js';
+import useProfile from '../../hooks/useProfile';
 
-const Avatar = ({ email }) => {
-  const hashedEmail = CryptoJS.SHA256(email);
+const Avatar = ({ profileId, type }) => {
+  const { profileResult, profileLoading, profileError } = useProfile(profileId);
+
+  const hashedEmail = CryptoJS.SHA256(profileResult?.profile.email);
   const gravatarUrl = `https://www.gravatar.com/avatar/${hashedEmail}?d=mp`;
+
+  if (profileLoading) {
+    return (
+      <div className={styles.loading}>
+        <div className={styles[`${type}Placeholder`]}></div>
+      </div>
+    );
+  }
+
+  if (profileError) {
+    return (
+      <div className={styles.Avatar}>
+        <div className={styles[`${type}Placeholder`]}></div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.Avatar}>
-      <img src={gravatarUrl}></img>
+      <img className={styles[`${type}Image`]} src={gravatarUrl}></img>
     </div>
   );
 };
 
 Avatar.propTypes = {
-  email: PropTypes.string,
+  profileId: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default Avatar;
