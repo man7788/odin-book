72677c9df68c5c2dee137882892b0cc6c83f8aeb
man7788 <jerman1841@gmail.com>
2024-12-04 01:18:41 +0800

    Update render avatar

--- client/src/components/avatar/Avatar.jsx
+++ client/src/components/avatar/Avatar.jsx
@@ -1,13 +1,25 @@
 import styles from './Avatar.module.css';
 import PropTypes from 'prop-types';
 import CryptoJS from 'crypto-js';
+import { useEffect, useState } from 'react';
 import useProfile from '../../hooks/useProfile';
 
 const Avatar = ({ profileId, type }) => {
   const { profileResult, profileLoading, profileError } = useProfile(profileId);
+  const [avatarUrl, setAvatarUrl] = useState('');
 
-  const hashedEmail = CryptoJS.SHA256(profileResult?.profile.email);
-  const gravatarUrl = `https://www.gravatar.com/avatar/${hashedEmail}?d=mp`;
+  useEffect(() => {
+    const regex = /github/;
+    const github = regex.test(profileResult?.profile.avatar);
+
+    if (github) {
+      setAvatarUrl(profileResult?.profile.avatar);
+    } else {
+      const hashedEmail = CryptoJS.SHA256(profileResult?.profile.avatar);
+      const gravatarUrl = `https://www.gravatar.com/avatar/${hashedEmail}?d=mp`;
+      setAvatarUrl(gravatarUrl);
+    }
+  }, [profileResult]);
 
   if (profileLoading) {
     return (
@@ -27,7 +39,7 @@ const Avatar = ({ profileId, type }) => {
 
   return (
     <div className={styles.Avatar}>
-      <img className={styles[`${type}Image`]} src={gravatarUrl}></img>
+      <img className={styles[`${type}Image`]} src={avatarUrl}></img>
     </div>
   );
 };
