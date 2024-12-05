039901db82f1b6811c71c35c1e591f550e84a9af
man7788 <jerman1841@gmail.com>
2024-12-05 08:24:48 +0800

    Update ESlint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,8 +3,23 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import vitest from '@vitest/eslint-plugin';
 
 export default [
+  {
+    files: '**/__tests__/**/*.whatever.[jt]s',
+    plugins: {
+      vitest,
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+      },
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
