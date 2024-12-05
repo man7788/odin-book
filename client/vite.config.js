aa336ebce7154d5e8530488cfd4e7d620b251733
man7788 <jerman1841@gmail.com>
2024-12-05 08:24:40 +0800

    Install Vitest

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
+});
+
+// https://stackoverflow.com/questions/73392328/vs-code-showing-eslint-error-but-vitest-is-working-vi-is-not-defined
+
+// With the eslint new flat config (eslint.config.js), this is the way:
+// import vitest from '@vitest/eslint-plugin';
+// ...
+// {
+//     files: '**/__tests__/**/*.whatever.[jt]s',
+//     plugins: {
+//         vitest
+//     },
+//     languageOptions: {
+//         globals: {
+//             ...vitest.environments.env.globals
+//         }
+//     },
+//     rules: {
+//         ...vitest.configs.recommended.rules
+//     }
+// }
