bc3496df505175cde865ecfffecd3fe0e1e8e217
man7788 <jerman1841@gmail.com>
2024-12-08 02:16:10 +0800

    Add render loading test

---
+++ client/src/components/avatar/Avatar.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import * as useProfile from '../../hooks/useProfile';
+import Avatar from './Avatar';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+const useProfileSpy = vi.spyOn(useProfile, 'default');
+
+describe('Avatar', () => {
+  test('should render loading', () => {
+    useProfileSpy.mockReturnValue({
+      profileResult: null,
+      profileLoading: true,
+      profileError: null,
+    });
+
+    const { container } = render(
+      <BrowserRouter>
+        <Avatar type={'foobar'} />
+      </BrowserRouter>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
