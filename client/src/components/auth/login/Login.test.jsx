bbc14bd871df6ca4f8df0770816438c424a88b77
man7788 <jerman1841@gmail.com>
2024-12-08 08:19:17 +0800

    Update render Login test

--- client/src/components/auth/login/Login.test.jsx
+++ client/src/components/auth/login/Login.test.jsx
@@ -1,14 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
 import Login from './Login';
+import * as useAuth from '../../../hooks/useAuth';
 
 afterEach(() => {
   vi.clearAllMocks();
 });
 
+const useAuthSpy = vi.spyOn(useAuth, 'default');
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    Navigate: vi.fn(({ to }) => `Redirected to ${to}`),
+  };
+});
+
 describe('Login', () => {
   test('should render Login', () => {
+    useAuthSpy.mockReturnValue({
+      authResult: null,
+      authLoading: false,
+      authError: true,
+    });
+
     const { container } = render(
       <BrowserRouter>
         <Login />
