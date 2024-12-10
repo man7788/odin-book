57f21cd25d9fcbdda7c0ecb9117a670ec47759cc
man7788 <jerman1841@gmail.com>
2024-12-11 07:59:58 +0800

    Add render loading test

---
+++ client/src/components/posts/Post.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import Post from './Post';
+import * as useSinglePost from '../../hooks/useSinglePost';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+const useSinglePostSpy = vi.spyOn(useSinglePost, 'default');
+
+describe('Post', () => {
+  test('should render loading', () => {
+    useSinglePostSpy.mockReturnValue({
+      postResult: null,
+      postLoading: true,
+      postError: null,
+    });
+
+    const { container } = render(<Post _id={'placeholder'} />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
