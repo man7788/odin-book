9f3d730939e38dd9c152e1d40dd5f8f946cff504
man7788 <jerman1841@gmail.com>
2024-12-10 12:39:13 +0800

    Add redirect to homepage test

--- client/src/components/auth/login/GitHubCallback.test.jsx
+++ client/src/components/auth/login/GitHubCallback.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { act } from 'react';
+import { waitFor } from '@testing-library/react';
 import GitHubCallback from './GitHubCallback';
+import * as githubFetch from '../../../fetch/githubFetch';
 
 afterEach(() => {
   vi.clearAllMocks();
@@ -14,16 +16,31 @@ vi.mock('react-router-dom', async (importOriginal) => {
   };
 });
 
+const githubFetchSpy = vi.spyOn(githubFetch, 'default');
 vi.spyOn(Storage.prototype, 'setItem');
 
 describe('GitHubCallback', () => {
   test('should render GithubCallback', () => {
-    const { container } = render(
-      <BrowserRouter>
-        <GitHubCallback />
-      </BrowserRouter>,
-    );
+    const { container } = render(<GitHubCallback />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test('should redirect to homepage', async () => {
+    githubFetchSpy.mockReturnValue({
+      result: { token: 'jwt' },
+    });
+
+    const { container } = render(<GitHubCallback />);
+
+    await waitFor(() => expect(githubFetchSpy).toHaveBeenCalledTimes(1));
+
+    await act(async () => {
+      expect(localStorage.setItem).toHaveBeenCalledWith(
+        'token',
+        JSON.stringify('jwt'),
+      );
+      expect(container).toMatchSnapshot();
+    });
   });
 });
