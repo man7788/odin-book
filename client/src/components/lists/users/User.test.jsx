2e3200d3a6e35d0cc78b1944516bffc8f6e7fc8b
man7788 <jerman1841@gmail.com>
2024-12-12 04:07:51 +0800

    Add render user follow button test

--- client/src/components/lists/users/User.test.jsx
+++ client/src/components/lists/users/User.test.jsx
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import User from './User';
 import * as useFollowing from '../../../hooks/useFollowing';
 
@@ -7,6 +9,16 @@ afterEach(() => {
 });
 
 const useFollowingSpy = vi.spyOn(useFollowing, 'default');
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    useOutletContext: () => ({
+      profile: 'foobar',
+    }),
+  };
+});
 
 describe('User', () => {
   test('should render loading', () => {
@@ -29,6 +41,22 @@ describe('User', () => {
     });
 
     const { container } = render(<User _id={'placeholder'} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should render user follow button', () => {
+    useFollowingSpy.mockReturnValue({
+      followingResult: { following: false },
+      followingLoading: false,
+      followingError: null,
+    });
+
+    const { container } = render(
+      <BrowserRouter>
+        <User _id={'placeholder'} full_name={'foobar'} />,
+      </BrowserRouter>,
+    );
 
     expect(container).toMatchSnapshot();
   });
