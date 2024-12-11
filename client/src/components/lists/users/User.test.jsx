236c5a4259e495ea2e411f57bf4a9b17f7bbdac4
man7788 <jerman1841@gmail.com>
2024-12-12 04:23:19 +0800

    Add render submit loading test

--- client/src/components/lists/users/User.test.jsx
+++ client/src/components/lists/users/User.test.jsx
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-
+import userEvent from '@testing-library/user-event';
 import User from './User';
 import * as useFollowing from '../../../hooks/useFollowing';
+import * as requestFetch from '../../../fetch/requestFetch';
 
 afterEach(() => {
   vi.clearAllMocks();
 });
 
 const useFollowingSpy = vi.spyOn(useFollowing, 'default');
+const requestFetchSpy = vi.spyOn(requestFetch, 'default');
 
 vi.mock('react-router-dom', async (importOriginal) => {
   const actual = await importOriginal();
@@ -91,5 +93,36 @@ describe('User', () => {
     );
 
     expect(container).toMatchSnapshot();
+  });
+
+  describe('Submit request', () => {
+    test('should render loading', async () => {
+      const user = userEvent.setup();
+
+      useFollowingSpy.mockReturnValue({
+        followingResult: { following: false },
+        followingLoading: false,
+        followingError: null,
+      });
+
+      requestFetchSpy.mockReturnValue({
+        error: null,
+        result: null,
+      });
+
+      const { container } = render(
+        <BrowserRouter>
+          <User _id={'placeholder'} full_name={'foobar'} />,
+        </BrowserRouter>,
+      );
+
+      const followButton = await screen.findByRole('button', {
+        name: /follow/i,
+      });
+
+      await user.click(followButton);
+
+      expect(container).toMatchSnapshot();
+    });
   });
 });
