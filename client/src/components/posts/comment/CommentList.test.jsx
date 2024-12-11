49ec72e38af71e79a718bb53fa14458960f8e212
man7788 <jerman1841@gmail.com>
2024-12-11 09:16:21 +0800

    Add render no comment test

---
+++ client/src/components/posts/comment/CommentList.test.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import CommentList from './CommentList';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+const comments = [
+  {
+    _id: 'comment_id1',
+    post: 'post_id',
+    profile: 'profile_id1',
+    author: 'foo bar',
+    text_content: 'good post',
+    __v: 0,
+  },
+  {
+    _id: 'comment_id2',
+    post: 'post_id',
+    profile: 'profile_id1',
+    author: 'foo bar',
+    text_content: 'good post',
+    __v: 0,
+  },
+];
+
+describe('CommentList', () => {
+  test('should render CommentList with no comment', () => {
+    const { container } = render(
+      <CommentList postId={'post_id1'} comments={[]} setRenderPost={vi.fn()} />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
