b1180d0d14cb9133e2a877c363f5bd07ad939299
man7788 <jerman1841@gmail.com>
2024-12-13 03:42:29 +0800

    Add show user input test

--- client/src/components/create/Create.test.jsx
+++ client/src/components/create/Create.test.jsx
@@ -1,10 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Create from './Create';
+import * as createFetch from '../../fetch/createFetch';
 
 afterEach(() => {
   vi.clearAllMocks();
 });
+
+const createFetchSpy = vi.spyOn(createFetch, 'default');
 
 describe('Create', () => {
   test('should render Create', () => {
@@ -27,5 +30,19 @@ describe('Create', () => {
     });
 
     expect(postButton.className).toMatch(/active/i);
+  });
+
+  describe('Create', () => {
+    test('should show user input', async () => {
+      const user = userEvent.setup();
+
+      render(<Create />);
+
+      const input = await screen.findByPlaceholderText("What's new?");
+
+      await user.type(input, 'placeholder');
+
+      expect(input).toHaveValue('placeholder');
+    });
   });
 });
