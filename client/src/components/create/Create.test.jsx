75ecf0f692cf5db5f730d6d603030a9c16e3175a
man7788 <jerman1841@gmail.com>
2024-12-13 03:54:12 +0800

    Add render form loading test

--- client/src/components/create/Create.test.jsx
+++ client/src/components/create/Create.test.jsx
@@ -44,5 +44,28 @@ describe('Create', () => {
 
       expect(input).toHaveValue('placeholder');
     });
+
+    test('should render form loading', async () => {
+      const user = userEvent.setup();
+
+      createFetchSpy.mockReturnValue({
+        error: null,
+        result: null,
+      });
+
+      const { container } = render(<Create />);
+
+      const input = await screen.findByPlaceholderText("What's new?");
+
+      await user.type(input, 'placeholder');
+
+      const postButton = await screen.findByRole('button', {
+        name: /post/i,
+      });
+
+      await user.click(postButton);
+
+      expect(container).toMatchSnapshot();
+    });
   });
 });
