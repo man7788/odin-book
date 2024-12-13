0a0a9f8b7623711849a000c2357d043a21a9bc33
man7788 <jerman1841@gmail.com>
2024-12-14 02:50:27 +0800

    Add likeFetch tests

---
+++ client/src/fetch/tests/likeFetch.test.jsx
@@ -0,0 +1,47 @@
+import likeFetch from '../likeFetch';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+window.global.fetch = vi.fn();
+const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+
+describe('likeFetch', () => {
+  test('should throw error', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 401,
+        statusText: 'Unauthorized',
+      }),
+    );
+
+    const result = await likeFetch();
+
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(result.error).toEqual(
+      expect.objectContaining({
+        message: 'Unauthorized',
+        code: 401,
+      }),
+    );
+  });
+
+  test('should return result', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({ response: 'foobar' }),
+      }),
+    );
+
+    const { result } = await likeFetch();
+
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(result).toEqual({ response: 'foobar' });
+  });
+});
