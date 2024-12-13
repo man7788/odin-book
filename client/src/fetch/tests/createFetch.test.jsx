f549f233ef1ff3b208258eaffc5c1968cea7e36e
man7788 <jerman1841@gmail.com>
2024-12-14 02:44:03 +0800

    Add createFetch tests

---
+++ client/src/fetch/tests/createFetch.test.jsx
@@ -0,0 +1,69 @@
+import createFetch from '../createFetch';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+window.global.fetch = vi.fn();
+const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+
+describe('createFetch', () => {
+  test('should throw bad request error', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 400,
+        statusText: 'Bad Request',
+        json: () =>
+          Promise.resolve({
+            errors: [{ msg: 'error message' }],
+          }),
+      }),
+    );
+
+    const result = await createFetch();
+
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(result.error).toEqual({
+      errors: [{ msg: 'error message' }],
+    });
+  });
+
+  test('should throw unauthorized error', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 401,
+        statusText: 'Unauthorized',
+      }),
+    );
+
+    const result = await createFetch();
+
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(result.error).toEqual(
+      expect.objectContaining({
+        message: 'Unauthorized',
+        code: 401,
+      }),
+    );
+  });
+
+  test('should return result', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({ response: 'foobar' }),
+      }),
+    );
+
+    const { result } = await createFetch();
+
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(result).toEqual({ response: 'foobar' });
+  });
+});
