81a3faed34ea94fc7579669f3b4dbf1ad8517828
man7788 <jerman1841@gmail.com>
2024-12-13 08:54:15 +0800

    Remove unused code

--- client/src/fetch/commentFetch.jsx
+++ client/src/fetch/commentFetch.jsx
@@ -20,7 +20,6 @@ const commentFetch = async (postId, commentPayload) => {
     }
 
     if (response.status >= 400) {
-      console.log(response.status);
       const error = new Error(response.statusText);
       error.code = response.status;
       throw error;
