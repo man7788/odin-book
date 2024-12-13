0cbdd127201e7b723c2d0dc09d8d1e5eb3ca30d9
man7788 <jerman1841@gmail.com>
2024-12-14 02:03:29 +0800

    Add useUsers tests

---
+++ client/src/hooks/useUsers.test.jsx
@@ -0,0 +1,76 @@
+import { renderHook } from '@testing-library/react';
+import { act } from 'react';
+import useUsers from './useUsers';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+window.global.fetch = vi.fn();
+const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+
+describe('useUsers', () => {
+  test('should return loading true', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    const { result } = renderHook(() => useUsers());
+
+    await act(async () => {
+      expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+      expect(result.current).toEqual({
+        usersResult: null,
+        usersLoading: true,
+        usersError: null,
+      });
+    });
+  });
+
+  test('should throw error', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 400,
+        statusText: 'Unauthorized',
+      }),
+    );
+
+    const { result } = renderHook(() => useUsers());
+
+    await act(async () => {
+      expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    });
+
+    expect(result.current).toEqual({
+      usersResult: null,
+      usersLoading: false,
+      usersError: expect.objectContaining({
+        message: 'Unauthorized',
+        code: 400,
+      }),
+    });
+  });
+
+  test('should return response', async () => {
+    getItemSpy.mockReturnValue(JSON.stringify({ token: 'foobar' }));
+
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({ response: 'foobar' }),
+      }),
+    );
+
+    const { result } = renderHook(() => useUsers());
+
+    await act(async () => {
+      expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    });
+
+    expect(result.current).toEqual({
+      usersResult: { response: 'foobar' },
+      usersLoading: false,
+      usersError: null,
+    });
+  });
+});
